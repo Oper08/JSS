e532958208ec8fd19ae266b2e9c198268df0f1a8
oper <seisenbaianuarr@gmail.com>
2024-11-11 10:51:57 +0500

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let score =prompt("1 ден 12 ге дейин сан енгизиниз")
+switch (san){
+    case "1":
+    alert("ай" + "январь")
+       break;
+       case "2":
+       alert("Ай")
+} 
+
+        
+        
+
